e750047a387100d033e3cc6e3d035b48d4ec28a0
Himanshu <mehra3228@gmail.com>
2023-01-29 15:51:55 +0530

    user search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import UserResults from "../components/Users/UserResults";
+import UserSearch from "../components/Users/UserSearch";
 
 export default function Home() {
   return (
   <>
   {/* search component */}
+  <UserSearch/>
   <UserResults/>
   </>)
 }
